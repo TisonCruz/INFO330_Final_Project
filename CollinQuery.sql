9b950c745b78ad86eb0dfb6657774cb6ba6354dd
collinshen123 <collinshen123@gmail.com>
2023-05-31 12:15:19 -0700

    first query

--- CollinQuery.sql
+++ CollinQuery.sql
@@ -1,5 +1,8 @@
-Select d.stl_pct, p.player_name
-From Defensive Table as d
-Join player_table
-Order By stl_pct desc
+Select pt.player_name, dt.stl_pct
+From defensive_Table as dt
+Join composite_table as ct on ct.unique_season_id = de.unique_season_id
+Join player_table as pt on pt.unique_id = mt.unique_id
+Order By dt.stl_pct desc
 Limit 10; 
+
+
