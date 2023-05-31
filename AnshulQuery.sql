912dddb1aab545f9fb889fbd79e41bd84452117a
anshulck03 <125226129+anshulck03@users.noreply.github.com>
2023-05-31 12:51:59 -0700

    updated queries

--- AnshulQuery.sql
+++ AnshulQuery.sql
@@ -2,3 +2,7 @@ SELECT player, MAX(pts_per_g) AS max_points
 FROM playoffStats
 GROUP BY player
 ORDER BY max_points DESC;
+
+SELECT pos, AVG(fg_pct) AS avg_fg_percentage
+FROM playoffStats
+GROUP BY pos;
